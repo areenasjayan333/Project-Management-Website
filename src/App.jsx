27e3a47b8dae2f80aec20716d43f889c8079508d
areenasjayan333 <areesjayan123@gmail.com>
2024-01-24 23:39:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import ProjectSideBar from "./components/projectSideBar";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import { useState } from "react";
+import NewProject from "./components/NewProject.jsx";
+
+
+function App() {
+ const [projectState,setProjectState] = useState({
+  selectedProjectId:undefined,
+  projects: []
+ });
+
+ function handleStartAddProject(){
+  setProjectState(prevState =>{
+    return {
+    ...prevState,
+    selectedProjectId: null,
+  };
+  });
+ }
+
+ let content;
+
+ if(projectState.selectedProjectId === null){
+content = <NewProject/>
+ }
+ else if (projectState.selectedProjectId === undefined) {
+  content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+ }
+
+ 
+  return (
+      <main className="h-screen my-8 flex gap-8">
+     <ProjectSideBar onStartAddProject={handleStartAddProject}/>
+     {content}
+      </main>
+  );
+}
+
+export default App;
